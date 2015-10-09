e9d9954e7334f3c8a900c50e958b8021430d6b34
RebeccaKuo <1146ra@gmail.com>
2015-10-09 19:31:41 +0100

    first commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main(void)
+{
+    string m("There is no water.");
+     
+    //cout << endl;
+    cout << m << endl;    
+    //cout << endl << endl;
+     
+    return 0;
+}
